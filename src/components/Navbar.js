183f09be80ece1c546bde94847520ecda8ed9de4
Yash-sudo-web <ymathur123@gmail.com>
2023-04-14 22:37:42 +0530

    upadate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,38 +2,27 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="/">Navbar</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/">Link</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="/">Action</a>
-          <a className="dropdown-item" href="/">Another action</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="/">Something else here</a>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <img src='logo (1).png'/>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">About</a>
+          </li>
+        </ul>
+        <div className="form-check form-switch text-light">
+  <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
         </div>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="/">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+      </div>
     </div>
+  </nav>
   )
 }
